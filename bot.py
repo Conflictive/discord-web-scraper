8c23553945d772702190f47c88944f4786d84b03
Callum Tolland <callumtolland@gmail.com>
2026-05-08 04:38:01 +0100

    format: refactor bot.py to be class based

--- bot.py
+++ bot.py
@@ -22,35 +22,36 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-# 2. Configure Intents: Required for reading message content in prefix commands
-intents = discord.Intents.default()
-intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+class Bot:
+    def __init__(self):
+        # 2. Configure Intents: Required for reading message content in prefix commands
+        intents = discord.Intents.default()
+        intents.message_content = True
 
+        self.bot = commands.Bot(command_prefix="!", intents=intents)
 
-async def load_extensions():
-    """Iterates through the cogs folder and registers each Python file as an extension."""
-    for filename in os.listdir("./cogs"):
-        # Skip __init__.py and non-python files
-        if filename.endswith(".py") and filename != "__init__.py":
-            # Load using dot-notation: cogs.filename
-            # filename[:-3] removes the file extension
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+    async def load_extensions(self):
+        """Iterates through the cogs folder and registers each Python file as an extension."""
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py") and filename != "__init__.py":
+                # Load using dot-notation: cogs.filename
+                # filename[:-3] removes the file extension
+                await self.bot.load_extension(f"cogs.{filename[:-3]}")
 
-
-async def main():
-    """Startup for the bot"""
-    async with bot:
-        await load_extensions()
-        await bot.start(TOKEN)
+    async def main(self):
+        """Startup for the bot"""
+        async with self.bot:
+            await self.load_extensions()
+            await self.bot.start(TOKEN)
 
 
 if __name__ == "__main__":
     import asyncio
 
     try:
-        asyncio.run(main())
+        bot = Bot()
+        asyncio.run(bot.main())
     except KeyboardInterrupt:
         #  Handle the bot being shut down via Ctrl+C
         print("Bot is shutting down...")
